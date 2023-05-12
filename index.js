eb351e4bf4d28ab76de460c4e6e9b8b8c12fdd4d
Greg Ejimofor <grgonyx@gmail.com>
2023-05-12 14:25:48 -0700

    commit tree 6e810322a99dba3d9cddc68188828daf8b57c193

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const header = document.querySelector('h1');
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "YOUR-NAME is the champion";
+header.replaceWith(newHeader);
